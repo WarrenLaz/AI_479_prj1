a1645d8653306d93a4e80bd371aac42f967dd102
Warren Lazarraga <79947453+WarrenLaz@users.noreply.github.com>
2025-01-24 23:37:42 -0500

    added UI

--- Mazeexe.py
+++ Mazeexe.py
@@ -4,7 +4,8 @@ import copy
 import math
 import heapq as h
 
-maze = Maze.createMaze(36,36,0.5)
+maze_0= Maze.createMaze(36,36,0.5)
+maze_ = maze_0[0]
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((720, 720))
@@ -40,55 +41,60 @@ def drawGrid(mazes):
     for x in range(0, screen.get_width(), blockSize):
         for y in range(0, screen.get_height(), blockSize):
             b = pygame.Rect(x, y, blockSize, blockSize)
-            if(mazes[0][int(x/blockSize)][int(y/blockSize)] == '.'):
+            if(mazes[int(x/blockSize)][int(y/blockSize)] == '.'):
                 pygame.draw.rect(screen, "white", b )
-            elif(mazes[0][int(x/blockSize)][int(y/blockSize)] == 'S'):
-                pygame.draw.rect(screen, "green", b )
-            elif(mazes[0][int(x/blockSize)][int(y/blockSize)] == 'G'):
+            elif(mazes[int(x/blockSize)][int(y/blockSize)] == 'G'):
+                pygame.draw.rect(screen, "purple", b )
+            elif(mazes[int(x/blockSize)][int(y/blockSize)] == '@'):
+                pygame.draw.rect(screen, "lightblue", b )
+            elif(mazes[int(x/blockSize)][int(y/blockSize)] == '*'):
                 pygame.draw.rect(screen, "blue", b )
+            elif(mazes[int(x/blockSize)][int(y/blockSize)] == 'S'):
+                pygame.draw.rect(screen, "green", b )
 
 
-l = len(maze)
-w = len(maze[0])
+l = len(maze_[1])
+w = len(maze_[0])
 visited = [[0 for _ in range(w)] for _ in range(l)]
-s = maze[1]
-g = maze[2]
-queue1 = []
-queue1.append((Heuristic(s[0], g[0], s[1], g[1]), s))
+s = maze_0[1]
+g = maze_0[2]
+queue1 = [(Heuristic(s[0], g[0], s[1], g[1]), s)]
 
-while queue1:
+while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-    h.heapify(queue1)
-    point = queue1[0][1]
-    x = point[1]
-    y = point[0]
-    listpoints = []
-    visited[y][x] = 1
-    listpoints = checkBounds(x, y, w, l, visited, maze, listpoints)
-    if(listpoints):
-        hlist = []
-        choice = []
-        for f in listpoints:
-            hlist.append(Heuristic(f[0], g[0], f[1], g[1]))
-        for i in range(len(listpoints)):
-            choice.append((hlist[i], listpoints[i]))
-        for c in choice:
-            if(not(c in queue1)):
-               queue1.append(c)
-            if(maze[c[1][0]][c[1][1]] == 'G'):
-                break
-            elif(not(maze[c[1][0]][c[1][1]] == 'S' or maze[c[1][0]][c[1][1]] == 'G')):
-                maze[c[1][0]][c[1][1]] = '@' 
-    if(queue1):
-        queue1.pop(0)
+    if queue1:
+        h.heapify(queue1)
+        point = queue1[0][1]
+        x = point[1]
+        y = point[0]
+        listpoints = []
+        visited[y][x] = 1
+        listpoints = checkBounds(x, y, w, l, visited, maze_, listpoints)
+        if (maze_[y][x] != 'S'): maze_[y][x] = '*'
+        if(listpoints):
+            hlist = []
+            choice = []
+            for f in listpoints:
+                hlist.append(Heuristic(f[0], g[0], f[1], g[1]))
+            for i in range(len(listpoints)):
+                choice.append((hlist[i], listpoints[i]))
+            for c in choice:
+                if(not(c in queue1)):
+                   queue1.append(c)
+                if(maze_[c[1][0]][c[1][1]] == 'G'):
+                    queue1.clear()
+                elif(not(maze_[c[1][0]][c[1][1]] == 'S' or maze_[c[1][0]][c[1][1]] == 'G')):
+                    maze_[c[1][0]][c[1][1]] = '@' 
+        if(queue1):
+            queue1.pop(0)
 
     screen.fill("black")
-    drawGrid(maze)
+    drawGrid(maze_)
     blockSizes = 20
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(10)
 
 pygame.quit()
